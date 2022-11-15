bae609731d734abad20089ded391dc107cf02b3d
lochana <lochana@DESKTOP-9U6DDHO.localdomain>
2022-11-15 20:22:49 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-const hello = () => {
-  console.log("Hello Node.js!");
-};
+const http = require('http')
+const fs = require('fs')
+// eslint-disable-next-line no-unused-vars
+const port = ''
+let homeContent = ''
+let projectContent = ''
+let regContent = ''
 
-hello();
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  homeContent = home
+})
+
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err
+  }
+  projectContent = project
+})
+
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    throw err
+  }
+  regContent = registration
+})
+
+http
+  .createServer((request, response) => {
+    const url = request.url
+    response.writeHeader(200, { 'Content-Type': 'text/html' })
+    switch (url) {
+      case '/project':
+        response.write(projectContent)
+        response.end()
+        break
+      case '/registration':
+        response.write(regContent)
+        response.end()
+        break
+      default:
+        response.write(homeContent)
+        response.end()
+        break
+    }
+  })
+  .listen(process.env.port)
