a00f07c2bb5c98657314e3a73e7794e6ae1b5b13
lochana <lochana@DESKTOP-9U6DDHO.localdomain>
2022-11-15 21:25:43 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-const http = require('http')
-const fs = require('fs')
-// eslint-disable-next-line no-unused-vars
-const port = ''
-let homeContent = ''
-let projectContent = ''
-let regContent = ''
-
-fs.readFile('home.html', (err, home) => {
-  if (err) {
-    throw err
-  }
-  homeContent = home
-})
-
-fs.readFile('project.html', (err, project) => {
-  if (err) {
-    throw err
-  }
-  projectContent = project
-})
-
-fs.readFile('registration.html', (err, registration) => {
-  if (err) {
-    throw err
-  }
-  regContent = registration
-})
-
-http
-  .createServer((request, response) => {
-    const url = request.url
-    response.writeHeader(200, { 'Content-Type': 'text/html' })
-    switch (url) {
-      case '/project':
-        response.write(projectContent)
-        response.end()
-        break
-      case '/registration':
-        response.write(regContent)
-        response.end()
-        break
-      default:
-        response.write(homeContent)
-        response.end()
-        break
+const http=require("http");
+const fs=require("fs");
+let homeContent="";
+let projectContent="";
+let registrationContent="";
+// eslint-disable-next-line no-undef
+const args = require("minimist")(process.argv.slice(2));
+fs.readFile("home.html",(err,home)=>{
+    if(err){ throw err};
+    homeContent=home.toString();
+});
+fs.readFile("project.html",(err,project)=>{
+    if(err){
+        throw err;
     }
-  })
-  .listen(process.env.port)
+    projectContent=project.toString();
+});
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err
+    }
+    registrationContent=registration.toString();
+});
+http.createServer((req,res)=>{
+    let url=req.url;
+    res.writeHead(200,{"content-type":"text/html"});
+    switch(url)
+    {
+        case "/registration":
+            res.write(registrationContent);
+            res.end();
+            break;
+        case "/project":
+            res.write(projectContent);
+            res.end();
+            break;
+        
+        default:res.write(homeContent);
+        res.end();
+        break;
+    }
+}).listen(args["port"]);
